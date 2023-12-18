b01d9abf1012199a7a88c525559e2a687ddace77
Zane Rockenbaugh <zane@liquid-labs.com>
2023-12-18 13:24:22 -0600

    test documentOptions and documentUsage

--- src/lib/lib/helpers.mjs
+++ src/lib/lib/helpers.mjs
@@ -3,11 +3,13 @@ const internalRef = (s) => s.replaceAll(/[^a-zA-Z0-9_-]+/g, '-').toLowerCase()
 const sectionMark = (depth) => '#'.repeat(depth)
 
 const separateOptions = (allOptions) => {
-  const defaultOptionIndex = allOptions.findIndex(({ defaultOption }) => defaultOption === true)
+  const defaultOptionIndex = allOptions === undefined
+    ? -1
+    : allOptions?.findIndex(({ defaultOption }) => defaultOption === true)
 
   const defaultOption = defaultOptionIndex === -1 ? undefined : allOptions[defaultOptionIndex]
 
-  const options = [...allOptions]
+  const options = [...(allOptions || [])]
   if (defaultOptionIndex !== -1) {
     options.splice(defaultOptionIndex, 1)
   }
---
+++ src/lib/lib/test/document-options.test.mjs
@@ -0,0 +1,30 @@
+/* global describe expect test */
+import { documentOptions } from '../document-options'
+
+describe('documentOptions', () => {
+  test.each([
+    [[], ''],
+    [[{ name: 'foo', defaultOption: true, description: 'The default option'}], ''],
+    [
+      [{ name: 'bar', description: 'Optional option'}], 
+      `## Test
+
+|Option|Description|
+|------|------|
+|\`--bar\`|Optional option|
+
+`
+    ],
+    [
+      [{name: 'baz', alias: 'b', description: 'Aliased option'}],
+      `## Test
+
+|Option|Description|
+|------|------|
+|\`--baz\`, \`-b\`|Aliased option|
+
+`
+    ]
+  ])('%s -> %s', (allOptions, expected) => 
+    expect(documentOptions({ allOptions, depth: 1, header: 'Test' })).toBe(expected))
+})
